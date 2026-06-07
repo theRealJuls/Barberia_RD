d5955b5c2e741c47760a1b41500e52e253800fa7
Julio Castillo <JulioCastillop71@gmail.com>
2026-06-07 19:30:58 -0400

    chore: backup antes de migrar a vite

--- front-end/src/supabaseClient.js
+++ front-end/src/supabaseClient.js
@@ -1,12 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://btpvpvdzvmvfyjditdpb.supabase.com'
-const supabaseKey = 'sb_publishable_zIHQJ1KsHy1MK7LGDE7UGA__Af61o-6'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
